72c67ca0ac72c71a94c67dafbc103819e47c8c1e
LOU TIANHAO <1169586278@qq.com>
2026-01-07 18:05:02 +0800

    update: desktop name

--- ui/ultrasound_tab.py
+++ ui/ultrasound_tab.py
@@ -624,7 +624,7 @@ class UltrasoundTab(QWidget):
         if x is None: return
         
         total_rotation = 2 * x
-        desktop_path = r"C:\Users\hkclr_user\Desktop"
+        desktop_path = r"C:\Users\admin\Desktop"
         timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
 
         # 判断是全局还是局部重建
