8a0483a9216d93399eca1c9599543921493e066a
LOU TIANHAO <1169586278@qq.com>
2026-01-19 14:56:19 +0800

    update: send_init_joint_position_command

--- ui/left_panel.py
+++ ui/left_panel.py
@@ -522,7 +522,7 @@ class LeftPanel(QWidget):
         self.tcp_manager.send_command(command)
 
     def send_init_joint_position_command(self):
-        J_STR = "-212.91,-57.72,60.74,94.30,-91.14,-41.23"
+        J_STR = "-237.7,-53.88,57.53,94.2,-90.18,-45.6"
         POS_ZERO = "0.00,0.00,0.00,0.00,0.00,0.00"
         cmd = f"WayPoint,0,{POS_ZERO},{J_STR},TCP,Base,50,360,0,0,1,0,0,0,ID1;"
         self.tcp_manager.send_command(cmd)
